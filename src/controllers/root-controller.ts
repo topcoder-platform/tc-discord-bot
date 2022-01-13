f9f461f58515175c407b24abcac692b4487a4594
Kiril Kartunov <kiril.kartunov@gmail.com>
2022-01-13 12:49:10 +0200

    add webhook for Thrive

--- src/controllers/root-controller.ts
+++ src/controllers/root-controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response, Router } from 'express';
 import router from 'express-promise-router';
+import fetch from 'node-fetch';
 
+import { Env } from '../services';
 import { Controller } from './controller';
 
 export class RootController implements Controller {
@@ -9,9 +11,29 @@ export class RootController implements Controller {
 
     public register(): void {
         this.router.get('/', (req, res) => this.get(req, res));
+        this.router.post('/webhooks/thrive', (req, res) => this.thriveWebhook(req, res));
     }
 
     private async get(req: Request, res: Response): Promise<void> {
         res.status(200).json({ name: 'Discord Bot Cluster API', author: 'Kiril Kartunov' });
+    }
+
+    /**
+     * Thrive webhook listener
+     * Acts upon Contentful webhooks
+     */
+    private async thriveWebhook(req: Request, res: Response): Promise<void> {
+        if (req.body.sys.revision === 1) {
+            const res = await fetch(Env.discordThriveWebhook, {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    content: `Hey @here, we have published a new article on Thrive. Have a look at it https://www.topcoder.com/thrive/articles/${req.body.fields.slug['en-US']}`
+                }),
+            });
+        }
+        res.status(200).end();
     }
 }
--- src/services/env.ts
+++ src/services/env.ts
@@ -4,5 +4,7 @@
  */
 export const Env = {
     token: process.env.DISCORD_BOT_TOKEN ?? '',
-    appId: process.env.DISCORD_APP_ID ?? ''
+    appId: process.env.DISCORD_APP_ID ?? '',
+    botChannelId: process.env.BOT_CHANNEL_ID ?? '',
+    discordThriveWebhook: process.env.DISCORD_THRIVE_WEBHOOK ?? ''
 }
